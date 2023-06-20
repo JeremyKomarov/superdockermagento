32ad5c15b08e9359dbbf6279e9ab0f979d0a6c7c
Jeremy <jeremyk@elad.co.il>
2023-06-20 07:58:56 +0300

    fixed get env logic

--- bin/get-env.sh
+++ bin/get-env.sh
@@ -1,49 +1,41 @@
 #!/bin/bash
 
-magentoVersion=$(jq -r '.require."magento/product-community-edition"' ./www/composer.json)
+magentoVersion=$(jq -r '.version' ./www/composer.json)
 
-sed -i '' -E "s/MAGENTO_VERSION=.*/MAGENTO_VERSION=$magentoVersion/" ./.env
+envFile="./.env"
+
+sed -i '' -E "s/MAGENTO_VERSION=.*/MAGENTO_VERSION=$magentoVersion/" "$envFile"
 
 if [[ $magentoVersion =~ ^(2.3.[0-2])$ ]]; then
-    sed -i '' -E "s/MARIADB_VERSION=.*/MARIADB_VERSION=10.2/" ./.env
-    sed -i '' -E "s/PHP_VERSION=.*/PHP_VERSION=7.2/" ./.env
-    echo "PHP VERSION 7.2"
-
+    mariadbVersion="10.2"
+    phpVersion="7.2"
 elif [[ $magentoVersion =~ ^(2.3.[3-6])$ ]]; then
-      if [[ $magentoVersion == "2.3.6" ]]; then
-          sed -i '' -E "s/MARIADB_VERSION=.*/MARIADB_VERSION=10.3/" ./.env
-          echo "MariaDB VERSION 10.3"
-      else
-          sed -i '' -E "s/MARIADB_VERSION=.*/MARIADB_VERSION=10.2/" ./.env
-          echo "MARIADB VERSION 10.2"
-      fi
-    sed -i '' -E "s/PHP_VERSION=.*/PHP_VERSION=7.3/" ./.env
-    echo "PHP VERSION 7.3"
-
+    if [[ $magentoVersion == "2.3.6" ]]; then
+        mariadbVersion="10.3"
+    else
+        mariadbVersion="10.2"
+    fi
+    phpVersion="7.3"
 elif [[ $magentoVersion =~ ^(2.3.7|2.4.[0-3])$ ]]; then
-      if [[ $magentoVersion == "2.3.7" ]]; then
-          sed -i '' -E "s/MARIADB_VERSION=.*/MARIADB_VERSION=10.3/" ./.env
-          echo "MariaDB VERSION 10.3"
-      else
-          sed -i '' -E "s/MARIADB_VERSION=.*/MARIADB_VERSION=10.4/" ./.env
-          echo "MARIADB VERSION 10.4"
-      fi
-    sed -i '' -E "s/PHP_VERSION=.*/PHP_VERSION=7.4/" ./.env
-    echo "PHP VERSION 7.4"
-
+    if [[ $magentoVersion == "2.3.7" ]]; then
+        mariadbVersion="10.3"
+    else
+        mariadbVersion="10.4"
+    fi
+    phpVersion="7.4"
 elif [[ $magentoVersion =~ ^(2.4.[4-5])$ ]]; then
-    sed -i '' -E "s/MARIADB_VERSION=.*/MARIADB_VERSION=10.4/" ./.env
-    sed -i '' -E "s/PHP_VERSION=.*/PHP_VERSION=8.1/" ./.env
-    echo "MARIADB VERSION 10.4"
-    echo "PHP VERSION 8.1"
-
+    mariadbVersion="10.4"
+    phpVersion="8.1"
 elif [[ $magentoVersion =~ ^(2.4.6)$ ]]; then
-    sed -i '' -E "s/MARIADB_VERSION=.*/MARIADB_VERSION=10.6/" ./.env
-    sed -i '' -E "s/PHP_VERSION=.*/PHP_VERSION=8.2/" ./.env
-    echo "MARIADB VERSION 10.6"
-    echo "PHP VERSION 8.2"
+    mariadbVersion="10.6"
+    phpVersion="8.2"
 fi
 
-
-
+sed -i '' -E "s/PROJECT_VERSION=.*/PROJECT_VERSION=$magentoVersion/" "$envFile"
 echo "Magento Version ($magentoVersion)"
+
+sed -i '' -E "s/PHP_VERSION=.*/PHP_VERSION=$phpVersion/" "$envFile"
+echo "PHP VERSION $phpVersion"
+
+sed -i '' -E "s/MARIADB_VERSION=.*/MARIADB_VERSION=$mariadbVersion/" "$envFile"
+echo "MariaDB VERSION $mariadbVersion"
